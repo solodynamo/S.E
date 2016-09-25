9f3180b7af73fb11ee054f7303762e6552d258c3
solodynamo <bittuf3@gmail.com>
2016-09-25 21:58:14 +0530

    Modals ctrl added

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -271,3 +271,23 @@ angular.module('stocker.controllers', [])
     };
   }
 ])
+.controller('LoginSignupCtrl', ['$scope', 'modalService', 'userService',
+  function($scope, modalService, userService) {
+
+    $scope.user = {email: '', password: ''};
+
+    $scope.closeModal = function() {
+      modalService.closeModal();
+    };
+
+    $scope.signup = function(user) {
+      userService.signup(user);
+    };
+
+    $scope.login = function(user) {
+      userService.login(user);
+    };
+  }
+])
+
+;
