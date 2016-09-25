00a68da730273ef8424c98452251a1d7200c7462
solodynamo <bittuf3@gmail.com>
2016-09-25 09:39:45 +0530

    Code refactoring

--- www/js/services.js
+++ www/js/services.js
@@ -172,59 +172,9 @@ angular.module('stocker.services', [])
             $ionicLoading.hide();
         }
 
-        var _showConfirm = function (data) {
-            var confirmPopup = $ionicPopup.confirm({
-                template: data.template,
-                cssClass: 'customConfirm'
-            });
-            return confirmPopup;
-        }
-
-        var _showAlert = function (data) {
-            var alertPopup = $ionicPopup.alert({
-                template: data.template,
-                cssClass: 'customAlert'
-            });
-            return alertPopup;
-        }
-
-        var _showPopup = function (data) {
-
-            var deferred = $q.defer();
-
-            $ionicPopup.show({
-                template: data.template,
-                title: data.title,
-                scope: data.scope,
-                cssClass: 'customPopup',
-                buttons: [{
-                    text: 'Cancel',
-                    onTap: function (e) {
-                        return false;
-                }}, {
-                    text: 'Submit',
-                    type: 'button-positive',
-                    onTap: function (e) {
-                        if (!data.scope[data.modelName].comment) {
-                            e.preventDefault();
-                        } else {
-                            return data.scope[data.modelName];
-                        }
-                }}]
-            }).then(function (response) {
-                deferred.resolve(response);
-            });
-
-            return deferred.promise;
-
-        }
-        
         return {
             _on: _on,
-            _off: _off,
-            _showConfirm: _showConfirm,
-            _showAlert: _showAlert,
-            _showPopup: _showPopup
+            _off: _off
         }
 
     })
