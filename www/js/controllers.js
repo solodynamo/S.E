7684699bf808d3c1b916327ddfdde5920db55d24
solodynamo <bittuf3@gmail.com>
2016-09-27 23:52:36 +0530

    signUp feature added

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -271,8 +271,8 @@ angular.module('stocker.controllers', [])
     };
   }
 ])
-.controller('loginSignupCtrl', ['$scope', 'modalService', 'Utils', 'Popup', '$ionicModal',
-  function($scope, modalService, Utils, Popup, $ionicModal) {
+.controller('loginCtrl', ['$scope', 'modalService', 'Utils', 'Popup', '$ionicModal', '$state', '$localStorage',
+  function($scope, modalService, Utils, Popup, $ionicModal, $state, $localStorage) {
 
      $scope.login = function(user) {
     if (angular.isDefined(user)) {
@@ -302,6 +302,7 @@ var loginWithFirebase = function(email, password) {
               });
               $state.go('app.myStocks');
             });
+            console.log("Its working");
           }
         });
         $localStorage.loginProvider = "Firebase";
@@ -337,3 +338,87 @@ var loginWithFirebase = function(email, password) {
 
  
   }])
+.controller('SignUpCtrl', ['$ionicModal', '$scope', 'Utils', 'Popup','$localStorage', '$state', 'modalService',function($ionicModal ,$scope, Utils, Popup, $localStorage, $state, modalService) {
+  $scope.closeModal= function() {
+    modalService.closeModal();
+  } 
+
+  $scope.$on('$ionicView.enter', function() {
+    //Clear the Registration Form.
+    $scope.user = {
+      email: '',
+      password: ''
+    };
+  })
+
+  $scope.register = function(user) {
+    //Check if form is filled up.
+    if (angular.isDefined(user)) {
+      Utils.show();
+      firebase.database().ref('accounts').orderByChild('email').equalTo(user.email).once('value').then(function(accounts) {
+        if (accounts.exists()) {
+          Utils.message(Popup.errorIcon, Popup.emailAlreadyExists);
+        } else {
+          //Create Firebase account.
+          firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+            .then(function() {
+              //Add Firebase account reference to Database. Firebase v3 Implementation.
+              firebase.database().ref().child('accounts').push({
+                email: user.email,
+                userId: firebase.auth().currentUser.uid,
+                dateCreated: Date(),
+                provider: 'Firebase'
+              }).then(function(response) {
+                //Account created successfully, logging user in automatically after a short delay.
+                Utils.message(Popup.successIcon, Popup.accountCreateSuccess)
+                  .then(function() {
+                    getAccountAndLogin(response.key);
+                  })
+                  .catch(function() {
+                    //User closed the prompt, proceed immediately to login.
+                    getAccountAndLogin(response.key);
+                  });
+                $localStorage.loginProvider = "Firebase";
+                $localStorage.email = user.email;
+                $localStorage.password = user.password;
+              });
+            })
+            .catch(function(error) {
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              //Show error message.
+              console.log(errorCode);
+              switch (errorCode) {
+                case 'auth/email-already-in-use':
+                  Utils.message(Popup.errorIcon, Popup.emailAlreadyExists);
+                  break;
+                case 'auth/invalid-email':
+                  Utils.message(Popup.errorIcon, Popup.invalidEmail);
+                  break;
+                case 'auth/operation-not-allowed':
+                  Utils.message(Popup.errorIcon, Popup.notAllowed);
+                  break;
+                case 'auth/weak-password':
+                  Utils.message(Popup.errorIcon, Popup.weakPassword);
+                  break;
+                default:
+                  Utils.message(Popup.errorIcon, Popup.errorRegister);
+                  break;
+              }
+            });
+        }
+      });
+    }
+  };
+
+  //Function to retrieve the account object from the Firebase database and store it on $localStorage.account.
+  getAccountAndLogin = function(key) {
+    firebase.database().ref('accounts/' + key).on('value', function(response) {
+      var account = response.val();
+      $localStorage.account = account;
+    });
+    $state.go('app.myStocks');
+  };
+
+
+}])
